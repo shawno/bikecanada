8ff4b4e8380f9dc3dccff809fe055ffdd01ec3cd
Shawn O'Neill <soneill83@gmail.com>
2015-06-01 21:30:25 -0700

    Added page title application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	#Returns the full title on a per-page basis.
+	def full_title(page_title = '')
+		base_title = "Shawn's Bike Trip Across Canada"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
