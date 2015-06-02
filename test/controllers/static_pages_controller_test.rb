e6bc7ea187dfb8b29eaa001cd75753d57d7425ab
Shawn O'Neill <soneill83@gmail.com>
2015-06-01 20:42:30 -0700

    Added some test reporting styling. Killed guard

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,16 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Shawn's Bike Trip Across Canada"
+  end
+
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Shawn's Bike Trip Across Canada"
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get gear" do
     get :gear
     assert_response :success
-    assert_select "title", "Gear | Shawn's Bike Trip Across Canada"
+    assert_select "title", "Gear | #{@base_title}"
   end
 
 end
