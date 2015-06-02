6d260e5851f8b2a873ce3cd33c77f63274341159
Shawn O'Neill <soneill83@gmail.com>
2015-06-01 20:29:25 -0700

    Initial static pages added

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Shawn's Bike Trip Across Canada"
   end
 
   test "should get gear" do
     get :gear
     assert_response :success
+    assert_select "title", "Gear | Shawn's Bike Trip Across Canada"
   end
 
 end
