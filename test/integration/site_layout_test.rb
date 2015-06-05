484b30296c8d7c80072efe799a463ec1ee38e092
Shawn O'Neill <soneill83@gmail.com>
2015-06-04 23:17:58 -0700

    Added bootstrap, header, unsupported browser message, and route names

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "site links" do
+    get root_path
+    assert_template 'static_pages/about'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", gear_path
+  end
+end
